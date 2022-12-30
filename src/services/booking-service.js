829d897f3f5722933f1c91b3ab9103852cfd1d13
Sachin goyal <goyalsachin176@gmail.com>
2022-12-30 23:25:29 +0530

    completed create booking

---
+++ src/services/booking-service.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+const { BookingRepository } = require('../repository/index');
+const { FLIGHT_SERVICE_PATH } = require('../config/serverConfig');
+const { ServiceError } = require('../utils/errors');
+
+class BookingService {
+    constructor() {
+        this.bookingRepository = new BookingRepository();
+    }
+
+    async createBooking(data) {
+        try {
+            const flightId = data.flightId;
+            const getFlightRequestURL = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${flightId}`; //we are making this api request to the flight service which is another service so using axios
+            const response = await axios.get(getFlightRequestURL);
+            const flightData = response.data.data;
+            let priceOfTheFlight = flightData.price;
+            if(data.noOfSeats > flightData.totalSeats) {
+                throw new ServiceError('Something went wrong in the booking process', 'Insufficient seats in the flight');
+            }
+            const totalCost = priceOfTheFlight * data.noOfSeats;
+            const bookingPayload = {...data, totalCost};
+            const booking = await this.bookingRepository.create(bookingPayload);
+            const updateFlightRequestURL = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${booking.flightId}`; //to updat the flights db also because the no of seats we have to decrease there
+            await axios.patch(updateFlightRequestURL, {totalSeats: flightData.totalSeats - booking.noOfSeats});
+            const finalBooking = await this.bookingRepository.update(booking.id, {status: "Booked"});  //to update the staus in booking table
+            return finalBooking;
+        } catch (error) { 
+            if(error.name == 'RepositoryError' || error.name == 'ValidationError') { //this is for if any error comes from the repo layer
+                throw error;
+            }
+            throw new ServiceError();
+        }
+    }
+}
+
+module.exports = BookingService;
