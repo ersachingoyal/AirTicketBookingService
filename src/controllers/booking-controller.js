4fe3951e440874e94736db1917d3fcda6adc5031
Sachin goyal <goyalsachin176@gmail.com>
2023-01-11 12:59:02 +0530

    Added rabbitmq more logic

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -15,8 +15,16 @@ class BookingController {
 
     async sendMessageToQueue(req, res) {
         const channel = await createChannel();
-        const data = {message: 'Success'};
-        publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+        const payload = {
+            data: {
+                subject: 'This is a noti from queue',
+                content: 'Some queue will subscribe this',
+                recepientEmail: 'goyalsachin171@gmail.com',
+                notificationTime: '2023-01-11T09:49:00'
+            },
+            service: 'CREATE_TICKET'
+        };
+        publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(payload));
         return res.status(200).json({
             message: 'Successfully published the event'
         });
--- src/utils/messageQueue.js
+++ src/utils/messageQueue.js
@@ -6,7 +6,7 @@ const createChannel = async () => {
         const connection = await amqplib.connect(MESSAGE_BROKER_URL); //first we have to create a connection
         const channel = await connection.createChannel(); //then we need to create a channel through that connection
         await channel.assertExchange(EXCHANGE_NAME, 'direct', false); //will create an exchange for the channel, the exchange checks to which queue the msg needs to be redirected as we can have multiple queues
-        //this msg redirection is done based on the binding the queue is having
+        //the producer which produces the msg sends it to the exchange or distributor we can and exchange decides to which queue the msg need to go, and this msg redirection is done based on the binding the queue is having
         return channel;
     } catch (error) {
         throw error;
@@ -15,7 +15,7 @@ const createChannel = async () => {
 
 const subscribeMessage = async (channel, service, binding_key) => {
     try {
-      const applicationQueue = await channel.assertQueue("QUEUE_NAME");
+      const applicationQueue = await channel.assertQueue("REMINDER_QUEUE");
       channel.bindQueue(applicationQueue.queue, EXCHANGE_NAME, binding_key);
 
       channel.consume(applicationQueue.queue, (msg) => {
@@ -29,9 +29,9 @@ const subscribeMessage = async (channel, service, binding_key) => {
     
 }
 
-const publishMessage = async (channel, binding_key, message) => {
+const publishMessage = async (channel, binding_key, message) => { //binding_key is used to redirect the msg to the respective queue, because we can have multiple queues simultaneously
     try {
-        await channel.assertQueue("QUEUE_NAME");
+        await channel.assertQueue("REMINDER_QUEUE");
         await channel.publish(EXCHANGE_NAME, binding_key, Buffer.from(message));
     } catch (error) {
         throw error;
